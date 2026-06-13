3fc4d98ef4052c087a42e81890858da96a30e064
vignesh <vigneshsai52@gmail.com>
2026-06-13 21:55:21 +0530

    Fix migration: remove DROP TABLE statement

--- db/migrations/001_create_users.sql
+++ db/migrations/001_create_users.sql
@@ -1,9 +1,5 @@
--- +migrate Up
 CREATE TABLE IF NOT EXISTS users (
     id   SERIAL PRIMARY KEY,
     name TEXT   NOT NULL,
     dob  DATE   NOT NULL
 );
-
--- +migrate Down
-DROP TABLE IF EXISTS users;
